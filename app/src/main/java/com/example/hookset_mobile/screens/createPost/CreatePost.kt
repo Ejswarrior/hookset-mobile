51fb75568e9dfe1bf8175d9f9038b60f829598d6
Erik Sabin <eriksabin97@gmail.com>
2025-01-08 07:57:34 -0500

    fix: fixed photo picker input along with added more inputs

--- app/src/main/java/com/example/hookset_mobile/screens/createPost/CreatePost.kt
+++ app/src/main/java/com/example/hookset_mobile/screens/createPost/CreatePost.kt
@@ -1,28 +1,41 @@
 package com.example.hookset_mobile.screens.createPost
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import org.koin.android.ext.android.inject
+import ui.components.HooksetInput.HooksetInput
 
 class CreatePost(navController: NavController): ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,23 +46,51 @@ class CreatePost(navController: NavController): ComponentActivity() {
             }
         }
     }
+    private val context: Context by inject()
+    private var description by mutableStateOf("")
 
-    val mediaPicker = registerForActivityResult(PickVisualMedia()) { uri ->
-        if(uri != null) Log.d("createPost", uri.toString())
-    }
 
 
     @Composable
     fun CreatePostScreen() {
-        Column(modifier = Modifier.padding(24.dp, 12.dp)) {
+        val mediaPicker = rememberLauncherForActivityResult(PickVisualMedia()) { uri ->
+            if(uri != null) Log.d("createPost", uri.toString())
+        }
+        val modifier = Modifier
+        Column(modifier = modifier
+            .padding(24.dp, 12.dp)
+            .verticalScroll(rememberScrollState())) {
+            Text(
+                text = "Provide Details About Your Catch",
+                color = Color.Black,
+                fontSize = 28.sp,
+                modifier = Modifier.padding(bottom = 48.dp),
+                textAlign = TextAlign.Center
+            )
             Button(
-                modifier = Modifier.height(140.dp).fillMaxWidth().align(Alignment.CenterHorizontally),
+                modifier = modifier
+                    .height(140.dp)
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally),
                 onClick = { mediaPicker.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly))},
                 shape = CutCornerShape(0.dp),
                 colors = ButtonDefaults.buttonColors(containerColor = Color.DarkGray)
             ) {
                 Icon(Icons.Filled.Add, "Add Post Icon", modifier = Modifier, Color.LightGray)
             }
+
+            Row(modifier = modifier.padding(0.dp, 24.dp, 0.dp, 0.dp)) {
+                HooksetInput(modifier = modifier, inputValue = description, onChange = {description = it}, hidden = false, multiLined = 10, minLines = 6, labelText = "Description" )
+            }
+
+            Row(modifier = modifier.padding(0.dp, 24.dp, 0.dp, 0.dp)) {
+                HooksetInput(modifier = modifier, inputValue = description, onChange = {description = it}, hidden = false, labelText = "Where was this caught?" )
+            }
+
+            Row(modifier = modifier.padding(0.dp, 24.dp, 0.dp, 0.dp)) {
+                HooksetInput(modifier = modifier, inputValue = description, onChange = {description = it}, hidden = false, labelText = "How much did it weigh?" )
+            }
+
         }
     }
 }
--- app/src/main/java/ui/components/HooksetInput.kt
+++ app/src/main/java/ui/components/HooksetInput.kt
@@ -1,22 +1,42 @@
 package ui.components.HooksetInput
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
-    @Composable
-   public fun HooksetInput(modifier: Modifier, labelText: String? = null, inputValue: String, onChange: (String) -> Unit, hidden: Boolean) {
+@Composable
+   public fun HooksetInput(
+        modifier: Modifier,
+        labelText: String? = null,
+        inputValue: String,
+        onChange: (String) -> Unit,
+        hidden: Boolean,
+        multiLined: Int? = null,
+        minLines: Int? = null,
+        numInput: Boolean? = false,
+   ) {
         Column(modifier.padding(bottom = 24.dp)) {
             if(labelText != null) {
                 Text(modifier = modifier.padding(bottom = 6.dp), text = labelText)
             }
-            TextField(value = inputValue, onValueChange = {onChange(it)}, modifier = modifier, maxLines = 1, minLines = 1,visualTransformation = if(!hidden) VisualTransformation.None  else PasswordVisualTransformation())
+            TextField(
+                value = inputValue,
+                onValueChange = {onChange(it)},
+                modifier = modifier.fillMaxWidth(),
+                maxLines = if(multiLined === null) 1 else multiLined,
+                minLines = if(minLines === null) 1 else minLines,
+                visualTransformation = if(!hidden) VisualTransformation.None  else PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
+            )
         }
     }
 
